4b50f57de8ba8524766106fc613395d647207296
dariusmaximus <dariulqa@wp.pl>
2020-04-30 20:07:41 +0200

    redux boilerplate setup

--- src/components/courses/CoursesPage.js
+++ src/components/courses/CoursesPage.js
@@ -1,6 +1,9 @@
 import React, { useState, useEffect } from 'react';
+import { connect } from 'react-redux';
+import * as courseActions from '../../redux/actions/courseActions';
+import PropTypes from 'prop-types';
 
-const CoursesPage = () => {
+const CoursesPage = ({ courses, createCourse }) => {
 
     const [course, setCourse] = useState({
         title: ""
@@ -14,14 +17,48 @@ const CoursesPage = () => {
         setCourse({ ...course, title: e.target.value });
     }
 
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        createCourse(course);
+        setCourse({ ...course, title: '' })
+    }
+
     return (
-        <form>
-            <h2>Courses</h2>
-            <h3>Add Course</h3>
-            <input type="text" onChange={(e) => handleChange(e)} value={course.title} />
-            <input type="submit" />
-        </form>
+        <div>
+            <form onSubmit={(e) => handleSubmit(e)}>
+                <h2>Courses</h2>
+                <h3>Add Course</h3>
+                <input type="text" onChange={(e) => handleChange(e)} value={course.title} />
+                <input type="submit" />
+            </form>
+
+            <ul>
+                {courses.map(course => (
+                    <li key={course.title}>{course.title}</li>
+                ))}
+            </ul>
+        </div>
     );
 };
 
-export default CoursesPage;
+CoursesPage.propTypes = {
+    courses: PropTypes.array.isRequired,
+    createCourse: PropTypes.func.isRequired
+}
+
+const mapStateToProps = (state) => {
+    return {
+        courses: state.courses
+    }
+}
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+        createCourse: course => dispatch(courseActions.createCourse(course))
+    }
+}
+
+export default connect(
+    mapStateToProps,
+    mapDispatchToProps
+)(CoursesPage);
--- src/index.js
+++ src/index.js
@@ -3,11 +3,16 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
 import './index.css';
 import App from './App';
+import configureStore from './redux/configureStore';
+import { Provider as ReduxProvider } from 'react-redux';
 
+const store = configureStore();
 
 ReactDOM.render(
-  <Router >
-    <App />
-  </Router>,
+  <ReduxProvider store={store}>
+    <Router >
+      <App />
+    </Router>
+  </ReduxProvider>,
   document.getElementById('root')
 );
